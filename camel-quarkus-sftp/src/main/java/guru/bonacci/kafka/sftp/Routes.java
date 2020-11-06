27cbc28234da9fbd2b818905a1e949577ddc268e
jeffrey_vanhelden <jeffrey.vanhelden@thewarehouse.co.nz>
2020-11-06 16:17:04 +1300

    ccloud credentials externalized

--- camel-quarkus-sftp/src/main/java/guru/bonacci/kafka/sftp/Routes.java
+++ camel-quarkus-sftp/src/main/java/guru/bonacci/kafka/sftp/Routes.java
@@ -1,20 +1,11 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package guru.bonacci.kafka.sftp;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Properties;
 
 import javax.enterprise.context.ApplicationScoped;
 
@@ -22,26 +13,34 @@ import org.apache.camel.builder.endpoint.EndpointRouteBuilder;
 
 import lombok.AllArgsConstructor;
 
-/**
- * Camel route definitions.
- */
-@AllArgsConstructor
 @ApplicationScoped
+@AllArgsConstructor
 public class Routes extends EndpointRouteBuilder {
 
-    @Override
+	@Override
     public void configure() throws Exception {
-    	final String brokers = "abc:9092";
-    	final String saslJaasConfig = "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"USERNAME\" password=\"PASSWORD\"";
-
-    	
+    	final Properties props = loadConfig("C:\\configs\\confluent\\cloud-test.properties");
+    	final String topic = "jeff-json";
+    			
         from(platformHttp("/fruits").httpMethodRestrict("POST"))
-        		.setBody().constant("blablbbbbbb")
-                .to(kafka("jeff-json")
-                   .brokers(brokers)
-                   .saslMechanism("PLAIN") 
-                   .securityProtocol("SASL_SSL")
-                   .sslEndpointAlgorithm("https")
-                   .saslJaasConfig(saslJaasConfig));
+        		.setBody().constant("dddddd")
+                .to(kafka(topic)
+                   .brokers(props.getProperty("bootstrap.servers"))
+                   .saslMechanism(props.getProperty("sasl.mechanism")) 
+                   .securityProtocol(props.getProperty("security.protocol"))
+                   .sslEndpointAlgorithm(props.getProperty("ssl.endpoint.identification.algorithm"))
+                   .saslJaasConfig(props.getProperty("sasl.jaas.config")));
     }
+
+	//TODO how to load external configs in Quarkus?
+    public static Properties loadConfig(final String configFile) throws IOException {
+		if (!Files.exists(Paths.get(configFile))) {
+			throw new IOException(configFile + " not found.");
+		}
+		final Properties cfg = new Properties();
+		try (InputStream inputStream = new FileInputStream(configFile)) {
+			cfg.load(inputStream);
+		}
+		return cfg;
+	}
 }
